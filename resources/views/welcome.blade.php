b6f9ba05b94878ad020ed83e3e33e41141cedd77
Edward Galligan <ed@divstrong.com>
2016-03-25 13:01:36 -0400

    Still trying to fix permission issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,6 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
-                <div>Added</div>
             </div>
         </div>
     </body>
