c0c1d79e4d7020a05ac0401ff8f20d31b7b6ade3
Edward <ed@divstrong.com>
2016-07-20 10:36:58 -0400

    Order events list by date

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -29,7 +29,7 @@ class EventsController extends ApiController
         $events = $this->event
                     ->with('promoter')
                     ->where('date_time', '>', date('Y-m-d H:i:s', strtotime('-1 week')))
-                    ->oderBy('date_time', 'asc')
+                    ->orderBy('date_time', 'asc')
                     ->get();
 
         return $this->respond([
