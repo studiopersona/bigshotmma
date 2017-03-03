0f854d7f8425f6064467e54cf871879a12f92972
Edward <ed@divstrong.com>
2017-03-03 11:44:20 -0500

    Change hero play now button and footer logo to use https

--- resources/views/partials/lander/hero.blade.php
+++ resources/views/partials/lander/hero.blade.php
@@ -2,6 +2,6 @@
 	<div class="content-container">
 		<h1 class="hero__header">Skills, Not Scripts.</h1>
 		<h2 class="hero__subheader">(we've got <a id="powerupsBtn" href="#" data-target="#powerups">power ups</a> too)</h2>
-		<a href="{{ env('APP_URL') }}/play" class="btn btn--outline hero__ctaBtn larger-screens-inline-block">Play Now!</a>
+		<a href="{{ url('/play') }}" class="btn btn--outline hero__ctaBtn larger-screens-inline-block">Play Now!</a>
 	</div>
 </section>
