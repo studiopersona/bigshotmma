20f9dc255d36c7da8f08b711e664f5211e1d58b6
Edward <ed@divstrong.com>
2017-05-08 18:47:41 -0400

    Fix withdrawl request errors

--- app/Http/Controllers/UserBalancesController.php
+++ app/Http/Controllers/UserBalancesController.php
@@ -19,19 +19,20 @@ class UserBalancesController extends Controller
     {
     	$user = \JWTAuth::parseToken()->authenticate();
 
-    	$balance = calculatePlayersBalance($this->userBalance, $user->id);
+    	$balance = calculatePlayersBalance($user->id);
 
-        if ( (int)$balance >= (int)$request->amount*100  ) {
+        if ( (float)$balance >= (float)$request->amount*100  ) {
 
 	    	$this->userBalance->create([
 	    		'user_id' => $user->id,
 	    		'transaction_type_id' => 3,
-	    		'amount' => (int)$request->amount*100,
+	    		'amount' => (float)$request->amount*100,
 	    		'is_credit' => 0,
 	    		'live_funds' => 0,
 	    	]);
+
 	    } else {
-	    	return reponse()->json(['failed' => true, 'msg' => 'You do not have sufficient funds to make this withdrawl.']);
+	    	return response()->json(['failed' => true, 'msg' => 'You do not have sufficient funds to make this withdrawl.']);
 	    }
 
     	event(new \Bsmma\Events\PlayerRequestedWithdrawl($user, $request->amount, $request->method));
